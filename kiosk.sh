775b4cf9594f1172bf7c732ba49cd85a360c8cb3
leomanfredini <leomanfredini@users.noreply.github.com>
2020-04-08 16:49:32 -0300

    Create kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ 
+# Impedir que o gerenciador de energia desligue ou apague a tela
+
+xset s noblank
+xset s off
+xset -dpms
+ 
+
+# Esconder o mouse da tela após 5 segundos de inatividade
+
+unclutter -idle 5 -root &
+
+
+# Limpar qualquer mensagem que aparecer na barra de aviso do Chromium
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+ 
+
+# Executar o Chromium 
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://monsta.farroupilha.rs.gov.br/ https://hetrixtools.com/r/bdf4aa4df66cdf3ee97993a1bfad0aff/ &
+ 
+
+# Alternar as abas abertas do Chromium a cada 20 segundos
+
+while true; do
+# Alterna abas sem dar reload
+xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+# Altera abas com reload
+# xdotool keydown ctrl+Tab; xdotool keydown ctrl+r; xdotool keyup ctrl+Tab; xdotool keyup ctrl+r;
+sleep 20
+done
