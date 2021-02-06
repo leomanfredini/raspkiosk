a9290bdfb8df79163dbd3faf18620a43374fd988
leomanfredini <leomanfredini@users.noreply.github.com>
2021-02-05 23:22:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,28 +4,35 @@
 # sudo wget -qO - https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/install.sh | bash
 
 # REMOVER PACOTES DESNECESSÁRIOS
-apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 smartsim java-common minecraft-pi libreoffice* -y
+sudo apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 smartsim java-common minecraft-pi libreoffice* -y
 #apt-get purge smartsim java-common minecraft-pi libreoffice* -y
 
 # LIMPAR ARQUIVOS RESIDUAIS
-apt-get clean && apt-get autoremove -y
+sudo apt-get clean && sudo apt-get autoremove -y
 
 # ATUALIZAR O SISTEMA
-apt-get update && apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
 # INSTALANDO PACOTES NECESSÁRIOS PARA O KIOSK MODE
-apt-get -y install xdotool unclutter sed wget
+sudo apt-get -y install xdotool unclutter sed wget
 
 # CRIAR O SCRIPT RESPONSÁVEL POR INICIAR O KIOSK MODE
 wget -P /home/pi https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/kiosk.sh
-chown pi:pi /home/pi/kiosk.sh
+#chown pi:pi /home/pi/kiosk.sh
 
 # CRIANDO O SERVIÇO PARA INICIAR O SCRIPT NO BOOT
-wget -P /lib/systemd/system https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/kiosk.service
+sudo wget -P /lib/systemd/system https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/kiosk.service
 
 # HABILITAR O SCRIPT NA INICIALIZAÇÃO
-systemctl enable kiosk.service
+sudo systemctl enable kiosk.service
 
+	echo
+	echo
+	echo
+	echo
+	echo
+	echo
+	echo
 	echo " ** Script RASPKIOSK instalado com sucesso **"
 	echo "Edite o arquivo /home/pi/kiosk.sh para alterar as URLs que serão exibidas em sequência"
 	echo
