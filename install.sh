9873864ac2f0d3e7c633d66f0a17269dd9839ae3
leomanfredini <leomanfredini@users.noreply.github.com>
2021-05-29 13:27:19 -0300

    Update install.sh
    Ajustes em alguns comentários

--- install.sh
+++ install.sh
@@ -4,26 +4,26 @@
 # sudo wget -qO - https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/install.sh | bash
 
 
-# REMOVER PACOTES DESNECESSÁRIOS
+# REMOVENDO PACOTES DESNECESSÁRIOS
 sudo apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 smartsim java-common minecraft-pi libreoffice* -y
 
-# LIMPAR ARQUIVOS RESIDUAIS
+# LIMPANDO ARQUIVOS RESIDUAIS
 sudo apt-get clean && sudo apt-get autoremove -y
 
-# ATUALIZAR O SISTEMA
+# ATUALIZANDO O SISTEMA
 sudo apt-get update && sudo apt-get upgrade -y
 
 # INSTALANDO PACOTES NECESSÁRIOS PARA O KIOSK MODE
 sudo apt-get -y install xdotool unclutter sed wget
 
-# CRIAR O SCRIPT RESPONSÁVEL POR INICIAR O KIOSK MODE
+# CRIANDO O SCRIPT RESPONSÁVEL POR INICIAR O KIOSK MODE
 wget -P /home/pi https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/kiosk.sh
 #chown pi:pi /home/pi/kiosk.sh
 
 # CRIANDO O SERVIÇO PARA INICIAR O SCRIPT NO BOOT
 sudo wget -P /lib/systemd/system https://raw.githubusercontent.com/leomanfredini/raspkiosk/master/kiosk.service
 
-# HABILITAR O SCRIPT NA INICIALIZAÇÃO
+# HABILITANDO O SCRIPT NA INICIALIZAÇÃO
 sudo systemctl enable kiosk.service
 
 	echo
@@ -34,7 +34,7 @@ sudo systemctl enable kiosk.service
 	echo
 	echo
 	echo " ** Script RASPKIOSK instalado com sucesso **"
-	echo "Edite o arquivo /home/pi/kiosk.sh para alterar as URLs que serão exibidas em sequência"
+	echo "Edite o arquivo /home/pi/kiosk.sh para alterar as URLs que serão exibidas em sequência pelo navegador de internet"
 	echo
 	echo "Comando para iniciar manualmente o Kiosk Mode:"
 	echo "  sudo systemctl start kiosk.service"
